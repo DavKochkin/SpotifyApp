1101c25f7c242b58d26325b38b5a28f59084ef62
David Kochkin <kochkin.david@gmail.com>
2023-10-30 16:00:56 +0200

    First commit

---
+++ SpotifyApp/Controllers/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SpotifyApp
+//
+//  Created by David Kochkin on 30.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Home"
+        view.backgroundColor = .red
+    }
+}
+
