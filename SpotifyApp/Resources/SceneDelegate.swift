b2e3e434567d0c0f779b0e486997d8edb4dd573c
David Kochkin <kochkin.david@gmail.com>
2023-10-30 19:12:32 +0200

    Setup AuthManager

--- SpotifyApp/Resources/SceneDelegate.swift
+++ SpotifyApp/Resources/SceneDelegate.swift
@@ -17,7 +17,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         let window = UIWindow(windowScene: windowScene)
         window.makeKeyAndVisible()
-        window.rootViewController = TabBarViewController()
+        
+        if AuthManager.shared.isSignedIn {
+            window.rootViewController = TabBarViewController()
+        } else {
+            let navVC = UINavigationController(rootViewController: WelcomeViewController())
+            navVC.navigationBar.prefersLargeTitles = true
+            navVC.viewControllers.first?.navigationItem.largeTitleDisplayMode = .always
+            window.rootViewController = navVC
+        }
+        
         self.window = window
     }
 
