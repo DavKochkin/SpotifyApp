84516310099f90ae98a6ffd32e593db3ddee492b
David Kochkin <kochkin.david@gmail.com>
2023-11-05 16:58:53 +0200

    Add userProfile model

--- SpotifyApp/Controllers/Core/HomeViewController.swift
+++ SpotifyApp/Controllers/Core/HomeViewController.swift
@@ -12,6 +12,17 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "gear"),
+                                                            style: .done,
+                                                            target: self,
+                                                            action: #selector(didTapSetting))
+    }
+    
+    @objc func didTapSetting() {
+        let vc = ProfileViewController()
+        vc.title = "Profile"
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
     }
 }
 
