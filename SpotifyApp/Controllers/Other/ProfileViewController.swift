c3a82f289cb2e3d21a926d0265998a653b6f8b09
David Kochkin <kochkin.david@gmail.com>
2023-11-06 18:35:19 +0200

    Add fetchProfile in ProfileVC

--- SpotifyApp/Controllers/Other/ProfileViewController.swift
+++ SpotifyApp/Controllers/Other/ProfileViewController.swift
@@ -8,17 +8,40 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Profile"
-        APIRCaller.shared.getCurrentUserProfile { result in
-            switch result {
-            case .success(let model):
-                break
-            case .failure(let error):
-                print(error.localizedDescription)
+        fetchProfile()
+        view.backgroundColor = .systemBackground
+    }
+    
+    private func fetchProfile() {
+        APIRCaller.shared.getCurrentUserProfile { [weak self] result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let model):
+                    self?.updateUI(with: model)
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    self?.failedToGetProfile()
+                }
             }
         }
+    }
+    
+    private func updateUI(with model: UserProfile) {
+        
+    }
+    
+    private func failedToGetProfile() {
+        let label = UILabel(frame: .zero)
+        label.text = "Failed to load profile"
+        label.sizeToFit()
+        label.textColor = .secondaryLabel
+        view.addSubview(label)
+        label.center = view.center
     }
 }
--- SpotifyApp/Controllers/Other/SettingsViewController.swift
+++ SpotifyApp/Controllers/Other/SettingsViewController.swift
@@ -14,14 +14,42 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         return tableView
     }()
+    
+    private var sections = [Section]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureModels()
         title = "Settings"
         view.backgroundColor = .systemBackground
         view.addSubview(tableView)
         tableView.delegate = self
         tableView.dataSource = self
+    }
+    
+    func configureModels() {
+        sections.append(Section(title: "Profile", options: [Option(title: "View your Profile", handler: { [weak self] in
+            DispatchQueue.main.async {
+                self?.viewProfile()
+            }
+        })]))
+        
+        sections.append(Section(title: "Account", options: [Option(title: "Sign Out", handler: { [weak self] in
+            DispatchQueue.main.async {
+                self?.signOutTapped()
+            }
+        })]))
+    }
+    
+    private func signOutTapped() {
+        
+    }
+    
+    private func viewProfile() {
+        let vc = ProfileViewController()
+        vc.title = "Profile"
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
     }
     
     override func viewDidLayoutSubviews() {
@@ -32,21 +60,28 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
     //MARK: TableView
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return sections.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return sections[section].options.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = sections[indexPath.section].options[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "Foo"
+        cell.textLabel?.text = model.title
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        //Call handler for cell
+        let model = sections[indexPath.section].options[indexPath.row]
+        model.handler()
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let model = sections[section]
+        return model.title
     }
 }
