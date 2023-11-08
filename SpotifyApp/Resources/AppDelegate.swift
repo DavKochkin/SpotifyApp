8fed46d81a0b41a44e9d0bab17b6474780d24dee
David Kochkin <kochkin.david@gmail.com>
2023-11-08 19:06:53 +0200

    Add App icon logo

--- SpotifyApp/Resources/AppDelegate.swift
+++ SpotifyApp/Resources/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window.makeKeyAndVisible()
         
         if AuthManager.shared.isSignedIn {
+            AuthManager.shared.refreshIfNeeded(completion: nil)
             window.rootViewController = TabBarViewController()
         } else {
             let navVC = UINavigationController(rootViewController: WelcomeViewController())
