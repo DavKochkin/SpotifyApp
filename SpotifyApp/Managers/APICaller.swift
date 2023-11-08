dfddb5084746de10a66f9e225605c2a9cd88f65b
David Kochkin <kochkin.david@gmail.com>
2023-11-08 19:31:07 +0200

    Add getNewReleases

--- SpotifyApp/Managers/APICaller.swift
+++ SpotifyApp/Managers/APICaller.swift
@@ -43,6 +43,26 @@ final class APIRCaller {
         }
     }
     
+    public func getNewReleases(completion: @escaping ((Result<String, Error>)) -> Void) {
+        createRequest(with: URL(string: Constants.baseAPIURL + "/browse/new-releases?limit=50"),
+                      type: .GET) { request in
+            let task = URLSession.shared.dataTask(with: request) { data, _, error in
+                guard let data = data, error == nil else {
+                    completion(.failure(APIError.failedToGetData))
+                    return
+                }
+                
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+                    print(json)
+                }
+                catch {
+                    completion(.failure(error))
+                }
+            }
+        }
+    }
+    
     //MARK: - Private
     
     enum HTTPMethod: String {
