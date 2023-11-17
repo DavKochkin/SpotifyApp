6494d5c83c50d662e56b54f76f6e5e3022869864
David Kochkin <kochkin.david@gmail.com>
2023-11-17 14:02:01 +0200

    Add getAlbumDetails

--- SpotifyApp/Managers/APICaller.swift
+++ SpotifyApp/Managers/APICaller.swift
@@ -20,6 +20,38 @@ final class APICaller {
         case failedToGetData
     }
     
+    //MARK: Albums
+    
+    public func getAlbumDetails(for album: Album, completion: @escaping (Result<String, Error>) -> Void) {
+        createRequest(
+            with: URL(string: Constants.baseAPIURL + "/albums/" + album.id),
+            type: .GET)
+        { request in
+            let task = URLSession.shared.dataTask(with: request) { data, _, error in
+                guard let data = data, error == nil else {
+                    completion(.failure(APIError.failedToGetData))
+                    return
+                }
+                
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data)
+                    print(json)
+                }
+                catch {
+                    print(error)
+                    completion(.failure(error))
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    //MARK: Playlist
+    
+    
+    
+    //MARK: - Profile
+    
     public func getCurrentUserProfile(completion: @escaping (Result<UserProfile, Error>) -> Void) {
         createRequest(
             with: URL(string: Constants.baseAPIURL + "/me"),
@@ -43,6 +75,8 @@ final class APICaller {
             task.resume()
         }
     }
+    
+    //MARK: Browse
     
     public func getNewReleases(completion: @escaping ((Result<NewReleasesResponse, Error>)) -> Void) {
         createRequest(with: URL(string: Constants.baseAPIURL + "/browse/new-releases?limit=50"), type: .GET) { request in
