f39308a7ab6d8e28f5e30456805bdd54a90c6258
David Kochkin <kochkin.david@gmail.com>
2023-11-19 13:29:32 +0200

    Add PlaylistDetails model

---
+++ SpotifyApp/Models/PlaylistDetailsResponse.swift
@@ -0,0 +1,24 @@
+//
+//  PlaylistDetailsResponse.swift
+//  SpotifyApp
+//
+//  Created by David Kochkin on 19.11.2023.
+//
+
+import Foundation
+
+struct PlaylistDetailsResponse: Codable {
+    let album_type: String
+    let artists: [Artist]
+    let available_markets: [String]
+    let external_urls: [String: String]
+    let id: String
+    let images: [APIImage]
+    let label: String
+    let name: String
+    let tracks: TracksResponse
+}
+
+struct TracksResponse: Codable {
+    let items: [AudioTrack]
+}
