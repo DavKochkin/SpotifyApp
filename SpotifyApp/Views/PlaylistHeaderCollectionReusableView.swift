f884489f5bc9fc7c719bc4b4208ea406c40a413d
David Kochkin <kochkin.david@gmail.com>
2023-11-21 12:27:10 +0200

    Fetch data

--- SpotifyApp/Views/PlaylistHeaderCollectionReusableView.swift
+++ SpotifyApp/Views/PlaylistHeaderCollectionReusableView.swift
@@ -19,12 +19,15 @@ final class PlaylistHeaderCollectionReusableView: UICollectionReusableView {
     
     private let descriptionLabel: UILabel = {
         let label = UILabel()
+        label.textColor = .secondaryLabel
         label.font = .systemFont(ofSize: 18, weight: .regular)
+        label.numberOfLines = 0
         return label
     }()
     
     private let ownerLabel: UILabel = {
         let label = UILabel()
+        label.textColor = .secondaryLabel
         label.font = .systemFont(ofSize: 18, weight: .light)
         return label
     }()
@@ -40,7 +43,7 @@ final class PlaylistHeaderCollectionReusableView: UICollectionReusableView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .red
+        backgroundColor = .systemBackground
         addSubview(imageView)
         addSubview(nameLabeL)
         addSubview(descriptionLabel)
@@ -53,9 +56,18 @@ final class PlaylistHeaderCollectionReusableView: UICollectionReusableView {
     
     override func layoutSubviews() {
         super.layoutSubviews()
+        let imageSize: CGFloat = height/1.8
+        imageView.frame = CGRect(x: (width-imageSize)/2, y: 20, width: imageSize, height: imageSize)
+        
+        nameLabeL.frame = CGRect(x: 10, y: imageView.bottom, width: width-20, height: 44)
+        descriptionLabel.frame = CGRect(x: 10, y: nameLabeL.bottom, width: width-20, height: 44)
+        ownerLabel.frame = CGRect(x: 10, y: descriptionLabel.bottom, width: width-20, height: 44)
     }
     
-    func configure(with: viewModel: String) {
-        
+    func configure(with viewModel: PlaylistHeaderViewModel) {
+        nameLabeL.text = viewModel.name
+        ownerLabel.text = viewModel.ownerName
+        descriptionLabel.text = viewModel.description
+        imageView.sd_setImage(with: viewModel.artworkURL)
     }
 }
