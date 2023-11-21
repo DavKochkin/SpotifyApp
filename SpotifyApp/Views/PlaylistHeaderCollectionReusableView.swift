ff99487eef245377f45c2904bdb080072420debb
David Kochkin <kochkin.david@gmail.com>
2023-11-21 11:54:34 +0200

    Setup PlaylistHeader

--- SpotifyApp/Views/PlaylistHeaderCollectionReusableView.swift
+++ SpotifyApp/Views/PlaylistHeaderCollectionReusableView.swift
@@ -11,10 +11,40 @@ import SDWebImage
 final class PlaylistHeaderCollectionReusableView: UICollectionReusableView {
         static let identifier = "PlaylistHeaderCollectionReusableView"
     
+    private let nameLabeL: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 22, weight: .semibold)
+        return label
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 18, weight: .regular)
+        return label
+    }()
+    
+    private let ownerLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 18, weight: .light)
+        return label
+    }()
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = UIImage(systemName: "photo")
+        return imageView
+    }()
+    
+    //MARK: Init
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
         backgroundColor = .red
+        addSubview(imageView)
+        addSubview(nameLabeL)
+        addSubview(descriptionLabel)
+        addSubview(ownerLabel)
     }
     
     required init?(coder: NSCoder) {
@@ -23,6 +53,9 @@ final class PlaylistHeaderCollectionReusableView: UICollectionReusableView {
     
     override func layoutSubviews() {
         super.layoutSubviews()
+    }
+    
+    func configure(with: viewModel: String) {
         
     }
 }
---
+++ SpotifyApp/Views/PlaylistHeaderViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PlaylistHeaderViewModel.swift
+//  SpotifyApp
+//
+//  Created by David Kochkin on 21.11.2023.
+//
+
+import Foundation
