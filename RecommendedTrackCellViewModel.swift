9fe7ea31ee1d5fde0332e02df487d28a110d730e
David Kochkin <kochkin.david@gmail.com>
2023-11-25 21:52:44 +0200

    Add SearchBar

---
+++ RecommendedTrackCellViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  RecommendedTrackCellViewModel.swift
+//  SpotifyApp
+//
+//  Created by David Kochkin on 14.11.2023.
+//
+
+import Foundation
+
+struct RecommendedTrackCellViewModel {
+    let name: String
+    let artistName: String
+    let artworkURL: URL?
+    
+}
---
+++ SpotifyApp/Views/AlbumCollectionViewCellViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  AlbumCollectionViewCellViewModel.swift
+//  SpotifyApp
+//
+//  Created by David Kochkin on 23.11.2023.
+//
+
+import Foundation
+import UIKit
+
+struct AlbumCollectionViewCellViewModel {
+    let name: String
+    let artistName: String
+}
