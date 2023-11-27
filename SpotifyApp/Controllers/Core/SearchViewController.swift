747929e54157528bdd812d8e30f20e5983a5d8e4
David Kochkin <kochkin.david@gmail.com>
2023-11-27 11:28:57 +0200

    Fetch label & image

---
+++ SpotifyApp/Controllers/Core/SearchViewController.swift
@@ -0,0 +1,109 @@
+//
+//  SearchViewController.swift
+//  SpotifyApp
+//
+//  Created by David Kochkin on 30.10.2023.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController, UISearchResultsUpdating {
+    
+    let searchController: UISearchController = {
+        let vc = UISearchController(searchResultsController: SearchResultsViewController())
+        vc.searchBar.placeholder = "Songs, Artists, Albums"
+        vc.searchBar.searchBarStyle = .minimal
+        vc.definesPresentationContext = true
+        return vc
+    }()
+
+    private let collectionView: UICollectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: UICollectionViewCompositionalLayout(sectionProvider: { _, _ in
+            let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1),
+                heightDimension: .fractionalHeight(1)))
+            
+            item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 7, bottom: 2, trailing: 7)
+            
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1),
+                heightDimension: .absolute(150)),
+                                                           subitem: item,
+                                                           count: 2)
+            
+            group.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 0)
+            
+            return NSCollectionLayoutSection(group: group)
+        }))
+    
+    private var categories = [Category]()
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        searchController.searchResultsUpdater = self
+        navigationItem.searchController = searchController
+        view.addSubview(collectionView)
+        collectionView.register(CategoryCollectionViewCell.self, forCellWithReuseIdentifier: CategoryCollectionViewCell.identifier)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .systemBackground
+        
+        APICaller.shared.getCategories { [weak self] result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let categories):
+                    self?.categories = categories
+                    self?.collectionView.reloadData()
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView.frame = view.bounds
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        guard  let resultsController = searchController.searchResultsController as? SearchResultsViewController,
+               let query = searchController.searchBar.text,
+               query.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+        // resultsContoller.update(with: results)
+        // Perform search
+        // APICaller.shared.search
+    }
+}
+
+
+
+extension SearchViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: CategoryCollectionViewCell.identifier,
+            for: indexPath) as? CategoryCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        let category = categories[indexPath.row]
+        cell.configure(with: CategoryCollectionViewCellViewModel(title: category.name,
+                                                                 artworkURL: URL(string: category.icons.first?.url ?? "")
+                                                                )
+        )
+        return cell
+    }
+}
---
+++ SpotifyApp/Controllers/Other/SearchResultsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchResultsViewController.swift
+//  SpotifyApp
+//
+//  Created by David Kochkin on 30.10.2023.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        
+    }
+}
