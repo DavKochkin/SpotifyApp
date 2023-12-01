d7c4e4e29a5c7a28609264831d262716cea6a51a
David Kochkin <kochkin.david@gmail.com>
2023-12-01 10:16:08 +0200

    Configure PlayerVC

--- SpotifyApp/Controllers/Other/PlayerViewController.swift
+++ SpotifyApp/Controllers/Other/PlayerViewController.swift
@@ -8,9 +8,36 @@
 import UIKit
 
 class PlayerViewController: UIViewController {
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = .systemBlue
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        view.addSubview(imageView)
+        configureBarButtons()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        imageView.frame = CGRect(x: 0, y: view.safeAreaInsets.top, width: view.width, height: view.height)
+    }
+    
+    private func configureBarButtons() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .close, target: self, action: #selector(didTapClose))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(didTapAction))
+    }
+    
+    @objc func didTapClose() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func didTapAction() {
+        // Actions
     }
 }
