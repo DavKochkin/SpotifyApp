58c1bb5d855fc98d7dbd9ca3575e5310d8740d59
David Kochkin <kochkin.david@gmail.com>
2023-12-08 19:22:37 +0200

    Add 2 VC

---
+++ SpotifyApp/Controllers/Other/Library/LibraryPlaylistsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LibraryPlaylistsViewController.swift
+//  SpotifyApp
+//
+//  Created by David Kochkin on 08.12.2023.
+//
+
+import UIKit
+
+class LibraryPlaylistsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+    
+}
