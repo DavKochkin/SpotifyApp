d034b07dc12d9e01655b79c3fa1274e859001f4f
David Kochkin <kochkin.david@gmail.com>
2023-12-09 11:21:12 +0200

    Add ScrollView

--- SpotifyApp/Controllers/Core/LibraryViewController.swift
+++ SpotifyApp/Controllers/Core/LibraryViewController.swift
@@ -8,9 +8,37 @@
 import UIKit
 
 class LibraryViewController: UIViewController {
+    
+    private let playlistsVC = LibraryPlaylistsViewController()
+    private let albumsVC = LibraryAlbumsViewController()
+    
+    private let scrollView: UIScrollView = {
+        let scroll = UIScrollView()
+        scroll.isPagingEnabled = true
+        return scroll
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        view.addSubview(scrollView)
+        
+        scrollView.delegate = self
+        scrollView.backgroundColor = .yellow
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        scrollView.frame = CGRect(x: 0,
+                                  y: view.safeAreaInsets.top+55,
+                                  width: view.width,
+                                  height: view.height-view.safeAreaInsets.top-view.safeAreaInsets.bottom-55)
+    }
+}
+
+
+extension LibraryViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
     }
 }
