c324fdd0cf870f4e5cf5ecd8bc6a2c1f196da97b
David Kochkin <kochkin.david@gmail.com>
2023-12-09 11:27:22 +0200

    Configure LibraryVC

--- SpotifyApp/Controllers/Core/LibraryViewController.swift
+++ SpotifyApp/Controllers/Core/LibraryViewController.swift
@@ -24,7 +24,8 @@ class LibraryViewController: UIViewController {
         view.addSubview(scrollView)
         
         scrollView.delegate = self
-        scrollView.backgroundColor = .yellow
+        scrollView.contentSize = CGSize(width: view.width*2, height: scrollView.height)
+        addChildren()
     }
     
     override func viewDidLayoutSubviews() {
@@ -33,6 +34,18 @@ class LibraryViewController: UIViewController {
                                   y: view.safeAreaInsets.top+55,
                                   width: view.width,
                                   height: view.height-view.safeAreaInsets.top-view.safeAreaInsets.bottom-55)
+    }
+    
+    private func addChildren() {
+        addChild(playlistsVC)
+        scrollView.addSubview(playlistsVC.view)
+        playlistsVC.view.frame = CGRect(x: 0, y: 0, width: scrollView.width, height: scrollView.height)
+        playlistsVC.didMove(toParent: self)
+        
+        addChild(albumsVC)
+        scrollView.addSubview(albumsVC.view)
+        albumsVC.view.frame = CGRect(x: view.width, y: 0, width: scrollView.width, height: scrollView.height)
+        albumsVC.didMove(toParent: self)
     }
 }
 
