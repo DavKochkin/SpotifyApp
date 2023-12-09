41ae67b221dad0862f1bd0a149a72c295e21f156
David Kochkin <kochkin.david@gmail.com>
2023-12-09 12:01:15 +0200

    Add indicatorView

--- SpotifyApp/Views/LibraryToggleView.swift
+++ SpotifyApp/Views/LibraryToggleView.swift
@@ -14,6 +14,13 @@ protocol LibraryToggleViewDelegate: AnyObject {
 
 class LibraryToggleView: UIView {
     
+    enum State {
+        case playlist
+        case album
+    }
+    
+    var state: State = .playlist
+    
     weak var delegate: LibraryToggleViewDelegate?
     
     private let playlistButton: UIButton = {
@@ -30,10 +37,19 @@ class LibraryToggleView: UIView {
         return button
     }()
     
+    private let indicatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemGreen
+        view.layer.masksToBounds = true
+        view.layer.cornerRadius = 4
+        return view
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubview(playlistButton)
         addSubview(albumsButton)
+        addSubview(indicatorView)
         
         playlistButton.addTarget(self, action: #selector(didTapPlaylists), for: .touchUpInside)
         albumsButton.addTarget(self, action: #selector(didTapAlbums), for: .touchUpInside)
@@ -44,23 +60,31 @@ class LibraryToggleView: UIView {
     }
     
     @objc func didTapPlaylists() {
+        state = .playlist
         delegate?.LibraryToggleViewDidTapPlaylists(self)
     }
     
     @objc func didTapAlbums() {
+        state = .album
         delegate?.LibraryToggleViewDidTapAlbums(self)
     }
 
     override func layoutSubviews() {
         super.layoutSubviews()
-        playlistButton.frame = CGRect(x: 0,
-                                      y: 0,
-                                      width: 100,
-                                      height: 50)
+        playlistButton.frame = CGRect(x: 0, y: 0, width: 100, height: 40)
+        albumsButton.frame = CGRect(x: playlistButton.right, y: 0, width: 100, height: 40)
         
-        albumsButton.frame = CGRect(x: playlistButton.right,
-                                      y: 0,
-                                      width: 100,
-                                      height: 50)
+        switch state {
+        case .playlist:
+            indicatorView.frame = CGRect(x: 0,
+                                         y: 50,
+                                         width: 100,
+                                         height: 3)
+        case .album:
+            indicatorView.frame = CGRect(x: 100,
+                                         y: 50,
+                                         width: 100,
+                                         height: 3)
+        }
     }
 }
