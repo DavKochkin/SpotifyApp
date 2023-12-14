c692392337be60fdbc0a9c78a459e721689b9186
David Kochkin <kochkin.david@gmail.com>
2023-12-14 13:07:52 +0200

    Add APICall deleteTrack

--- SpotifyApp/Managers/APICaller.swift
+++ SpotifyApp/Managers/APICaller.swift
@@ -154,6 +154,9 @@ final class APICaller {
                     if let response = results as? [String: Any], response["shapshot_id"] as? String != nil {
                         completion(true)
                     }
+                    else {
+                        completion(false)
+                    }
                 } catch {
                     completion(false)
                 }
@@ -164,7 +167,38 @@ final class APICaller {
     }
     
     public func removeTrackFromPlaylist(track: AudioTrack, playlist: Playlist, completion: @escaping (Bool) -> Void) {
-        
+        createRequest(with: URL(string: Constants.baseAPIURL + "/playlists/\(playlist.id)/tracks"), type: .DELETE) { baseRequest in
+            var request = baseRequest
+            let json: [String: Any] = [
+                "tracks": [
+                    [
+                        "uri": "spotify:track:\(track.id)"
+                    ]
+                ]
+            ]
+            request.httpBody = try? JSONSerialization.data(withJSONObject: json, options: .fragmentsAllowed)
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            let task = URLSession.shared.dataTask(with: request) { data, _, error in
+                guard let data = data, error == nil else {
+                    completion(false)
+                    return
+                }
+                
+                do {
+                    let results = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+                    if let response = results as? [String: Any], response["shapshot_id"] as? String != nil {
+                        completion(true)
+                    }
+                    else {
+                        completion(false)
+                    }
+                } catch {
+                    completion(false)
+                }
+                
+            }
+            task.resume()
+        }
     }
     
     
@@ -372,6 +406,7 @@ final class APICaller {
     enum HTTPMethod: String {
         case GET
         case POST
+        case DELETE
     }
     
     private func createRequest(with url: URL?,
